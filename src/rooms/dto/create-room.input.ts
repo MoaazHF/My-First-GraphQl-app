b7455a4887fea67f9d220ec75a0af2ae54bad6d6
Moaaz <MoazHasanFarouk@gmail.com>
2026-02-18 10:51:51 +0200

    made the create-room.input

--- src/rooms/dto/create-room.input.ts
+++ src/rooms/dto/create-room.input.ts
@@ -1,7 +1,48 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field, Float, Int } from '@nestjs/graphql';
+import {
+  IsArray,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+import { RoomType } from '../room-type.enum';
 
 @InputType()
 export class CreateRoomInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty({ message: 'The name is required' })
+  @IsString()
+  name: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'The description is required' })
+  @IsString()
+  description: string;
+
+  @IsNotEmpty({ message: 'The images are required' })
+  @Field(() => [String])
+  @IsArray()
+  @IsString({ each: true })
+  images: string[];
+
+  @IsNotEmpty({ message: 'The pricePerNight is required' })
+  @Field(() => Float)
+  @Min(0)
+  @IsNumber()
+  pricePerNight: number;
+
+  @Field(() => Int)
+  @IsInt()
+  @IsNotEmpty()
+  @Min(1)
+  capacity: number;
+
+  @Field(() => RoomType, { nullable: true })
+  @IsEnum(RoomType)
+  @IsOptional()
+  roomType?: RoomType;
 }
